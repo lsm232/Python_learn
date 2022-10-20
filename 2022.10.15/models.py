629cb3fc751ac19ab8517d89482892b3798bfb44
lsm232 <1255567784@qq.com>
2022-10-20 20:50:40 +0800

    normal

--- 2022.10.15/models.py
+++ 2022.10.15/models.py
@@ -5,11 +5,73 @@ from typing import List
 
 def create_modules(module_defs:list,img_size):
     img_size=[img_size]*2 if isinstance(img_size,int) else img_size
-    module_defs.pop(0)
-    output_filters=[3]
     module_list=nn.ModuleList()
+    out_filters=[3]
+    yolo_index=-1
     routs=[]
-    yolo_index=-1  #???
+    module_defs.pop(0)
+
+    for i,mdef in enumerate(module_defs):
+        modules=nn.Sequential()
+        if mdef["type"]=="convolutional":
+            kersize=mdef["size"]
+            stride=mdef["stride"]
+            filters=mdef["filters"]
+            bn=mdef["batch_normalize"]
+
+            modules.add_module("Conv2d",nn.Conv2d(out_filters[-1],filters,kernel_size=kersize,stride=stride,padding=kersize//2 if mdef["pad"] else 0,bias=not bn))
+
+            if bn:
+                modules.add_module("Batchnorm2d",nn.BatchNorm2d(filters))
+            else:
+                routs.append(i)
+
+            if mdef["activation"]=="leaky":
+                modules.add_module("activation",nn.LeakyReLU(0.1,True))
+            else:
+                pass
+
+        elif mdef["type"]=="maxpool":
+            kersize=mdef["size"]
+            stride=mdef["stride"]
+            modules=nn.MaxPool2d(kernel_size=kersize,stride=stride,padding=(kersize-1)//2)
+
+        elif mdef["type"]=="upsample":
+            modules=nn.Upsample(scale_factor=mdef["stride"])
+
+        elif mdef["type"]=="route":
+            layers=mdef["layers"]
+            filters=sum([out_filters[l if l<0 else l+1]for l in layers])
+            routs.extend([i+l if l<0 else l for l in layers])
+            modules=FeatureConcat(layers=layers)
+
+        elif mdef["type"]=="shortcut":
+            layers=mdef["from"]
+            filters=out_filters[-1]
+            routs.append(i+layers[0])
+            modules=WeightedFeatureFusion(layers=layers)
+
+        elif mdef["type"]=="yolo":
+            yolo_index+=1
+            stride=[32,16,8]
+            modules = YOLOLayer(anchors=mdef["anchors"][mdef["mask"]],  # anchor list
+                                nc=mdef["classes"],  # number of classes
+                                img_size=img_size,
+                                stride=stride[yolo_index])
+
+        else:
+            print("Warning: Unrecognized Layer Type: " + mdef["type"])
+
+        module_list.append(modules)
+        out_filters.append(filters)
+
+    routs_binary=[False]*len(module_defs)
+    for i in routs:
+        routs_binary[i]=True
+    return module_list,routs_binary
+
+
+
 
 
 
