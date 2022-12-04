1cdea46a494a30cdab459ab0ffbbd46be7fdac3d
lsm232 <1255567784@qq.com>
2022-12-04 19:22:13 +0800

    bad

--- 2022.12.01/ls.py
+++ 2022.12.01/ls.py
@@ -1,44 +1,32 @@
+def nms(boxes,scores,thresh=0.5):
+    x1=boxes[:,0]
+    y1=boxes[:,1]
+    x2=boxes[:,2]
+    y2=boxes[:,3]
 
+    _,order=scores.sort(0,descending=True)
+    keep=[]
+    while order.numel()>0:
+        if order.numel()==1:
+            keep.append(order[0])
+            break
+        else:
+            keep.append(order[0])
+            xx1=x1[order[1:]].clamp(min=x1[0])
+            yy1=y1[order[1:]].clamp(min=y1[0])
+            xx2 = x2[order[1:]].clamp(max=x2[0])
+            yy2 = y2[order[1:]].clamp(max=y2[0])
 
+            inter=(yy2-yy1).clamp(min=0)*(xx2-xx1).clamp(min=0)
+            ious=inter/(x1*y1+x2*y2-inter)
+            idx=(ious<=thresh).nonzero().squeeze()
 
-from datasets import load_dataset
-from noise2noise import Noise2Noise
-from argparse import ArgumentParser
+            if idx.numel()==0:
+                break
+            else:
+                order=order[idx+1]
+    return keep
 
-
-def parse_args():
-    """Command-line argument parser for training."""
-
-    # New parser
-    parser = ArgumentParser(description='PyTorch implementation of Noise2Noise from Lehtinen et al. (2018)')
-
-    # Data parameters
-    parser.add_argument('-t', '--train-dir', help='training set path', default='./../data/train')
-    parser.add_argument('-v', '--valid-dir', help='test set path', default='./../data/valid')
-    parser.add_argument('--ckpt-save-path', help='checkpoint save path', default='./../ckpts')
-    parser.add_argument('--ckpt-overwrite', help='overwrite model checkpoint on save', action='store_true')
-    parser.add_argument('--report-interval', help='batch report interval', default=500, type=int)
-    parser.add_argument('-ts', '--train-size', help='size of train dataset', type=int)
-    parser.add_argument('-vs', '--valid-size', help='size of valid dataset', type=int)
-
-    # Training hyperparameters
-    parser.add_argument('-lr', '--learning-rate', help='learning rate', default=0.001, type=float)
-    parser.add_argument('-a', '--adam', help='adam parameters', nargs='+', default=[0.9, 0.99, 1e-8], type=list)
-    parser.add_argument('-b', '--batch-size', help='minibatch size', default=4, type=int)
-    parser.add_argument('-e', '--nb-epochs', help='number of epochs', default=100, type=int)
-    parser.add_argument('-l', '--loss', help='loss function', choices=['l1', 'l2', 'hdr'], default='l1', type=str)
-    parser.add_argument('--cuda', help='use cuda', action='store_true')
-    parser.add_argument('--plot-stats', help='plot stats after every epoch', action='store_true')
-
-    # Corruption parameters
-    parser.add_argument('-n', '--noise-type', help='noise type',
-        choices=['gaussian', 'poisson', 'text', 'mc'], default='gaussian', type=str)
-    parser.add_argument('-p', '--noise-param', help='noise parameter (e.g. std for gaussian)', default=50, type=float)
-    parser.add_argument('-s', '--seed', help='fix random seed', type=int)
-    parser.add_argument('-c', '--crop-size', help='random crop size', default=128, type=int)
-    parser.add_argument('--clean-targets', help='use clean targets for training', action='store_true')
-
-    return parser.parse_args()
 
 
 
