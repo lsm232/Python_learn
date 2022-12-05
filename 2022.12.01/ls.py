116956f09a8b411c1e3b60f376ed152c780539b7
lsm232 <1255567784@qq.com>
2022-12-05 20:27:34 +0800

    bad

--- 2022.12.01/ls.py
+++ 2022.12.01/ls.py
@@ -1,32 +1,13 @@
-def nms(boxes,scores,thresh=0.5):
-    x1=boxes[:,0]
-    y1=boxes[:,1]
-    x2=boxes[:,2]
-    y2=boxes[:,3]
+import torch
+import torch.nn as nn
 
-    _,order=scores.sort(0,descending=True)
-    keep=[]
-    while order.numel()>0:
-        if order.numel()==1:
-            keep.append(order[0])
-            break
-        else:
-            keep.append(order[0])
-            xx1=x1[order[1:]].clamp(min=x1[0])
-            yy1=y1[order[1:]].clamp(min=y1[0])
-            xx2 = x2[order[1:]].clamp(max=x2[0])
-            yy2 = y2[order[1:]].clamp(max=y2[0])
-
-            inter=(yy2-yy1).clamp(min=0)*(xx2-xx1).clamp(min=0)
-            ious=inter/(x1*y1+x2*y2-inter)
-            idx=(ious<=thresh).nonzero().squeeze()
-
-            if idx.numel()==0:
-                break
-            else:
-                order=order[idx+1]
-    return keep
-
-
-
-
+def add_tcb(cfg):
+    feature_up_layers=[]
+    feature_bottom_layers=[]
+    feature_de_layers=[]
+    for i,k in enumerate(cfg):
+        feature_up_layers.append([nn.Conv2d(cfg[i],256,kernel_size=3,padding=1,stride=1,bias=False),nn.ReLU(True),nn.Conv2d(256,256,kernel_size=3,stride=1,padding=1)])
+        feature_bottom_layers.append([nn.ReLU(True),nn.Conv2d(256,256,kernel_size=3,padding=1),nn.ReLU(True)])
+        if i!=0:
+            feature_de_layers.append(nn.ConvTranspose2d(256,256,4,2))
+    return feature_up_layers,feature_bottom_layers,feature_de_layers
