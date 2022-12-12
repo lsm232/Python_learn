caf73b4bd857558776750c5fac787b127454fe4c
lsm232 <1255567784@qq.com>
2022-12-12 08:29:50 +0800

    bad

--- ga_rpn/feature_adption.py
+++ ga_rpn/feature_adption.py
@@ -16,4 +16,17 @@ class FeatureAdaption(BaseModule):
                      override=dict(type='Normal', name='conv_adaption', std=0.01))):
         super(FeatureAdaption, self).__init__(init_cfg)
         offset_channels=kernel_size*kernel_size*2
+        self.conv_offset=nn.Conv2d(2,deform_groups*offset_channels,1,bias=False)
+        self.conv_adaption=DeformConv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=(kernel_size-1)//2,
+            deform_groups=deform_groups,
+        )
+        self.relu=nn.ReLU(True)
+    def forward(self,x,shape):
+        offset=self.conv_offset(shape.detach())
+        x=self.relu(self.conv_adaption(x,offset))
+        return x
 
