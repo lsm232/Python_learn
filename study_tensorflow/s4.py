75e8b460b3970232a442ecfaa7a8e91999a6d48f
lsm232 <1255567784@qq.com>
2022-12-31 20:16:57 +0800

    bad

--- study_tensorflow/s4.py
+++ study_tensorflow/s4.py
@@ -1,35 +1,6 @@
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
 
-tx=np.linspace(0,10,100)
-ty=2*tx+np.random.rand(*tx.shape)
-
-X=tf.placeholder('float')
-Y=tf.placeholder('float')
-w=tf.Variable(tf.random_normal([1]),name='weight')
-b=tf.Variable(tf.zeros(1),name='bias')
-z=tf.multiply(w,X)+b
-
-loss=tf.reduce_mean(tf.square(z-Y))
-optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
-
-init=tf.global_variables_initializer()
-epochs=20
-display_freq=2
-
-with tf.Session() as sess:
-    sess.run(init)
-    plotdata={'batchsize':[],'loss':[]}
-    for epoch in range(epochs):
-        for (x,y) in zip(tx,ty):
-            sess.run(optimizer,feed_dict={X:x,Y:y})
-
-            if epoch%display_freq==0:
-                los=sess.run(loss,feed_dict={X:tx,Y:ty})
-                print('epoch:',epoch+1,'cost:',los,'w:',sess.run(w),'b=',sess.run(b))
-                if not (los=='NA'):
-                    plotdata['batchsize'].append(epoch)
-                    plotdata['loss'].append(los)
-
-    print('ok')
+c=tf.get_default_graph()
+print(c)
+with tf.Graph().as_default():
+    print(tf.get_default_graph())
