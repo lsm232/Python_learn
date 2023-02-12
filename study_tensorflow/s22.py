bf6b81ea740f59946f99e6992bd3d2506e15e4d3
lsm232 <1255567784@qq.com>
2023-02-12 22:51:22 +0800

    bad

--- study_tensorflow/s22.py
+++ study_tensorflow/s22.py
@@ -2,11 +2,20 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-from .cifar10 import *
+from study_tensorflow.cifar10 import *
 import pylab
 
 batch_size=128
 data_dir=r'J:\play\cifar-10-python'
 images_test,labels_test=inputs(eval_data=True,data_dir=data_dir,batch_size=batch_size)
 
+sess=tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.train.start_queue_runners()
+images_batch,labels_batch=sess.run([images_test,labels_test])
+print(images_batch.shape)
+print(labels_batch.shape)
+c=images_batch[0].max()-images_batch[0].min()
+plt.imshow((images_batch[0]-images_batch[0].min())/c)
+plt.show()
 
---
+++ study_tensorflow/s23.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pylab
+
+
+filename=r'J:\play\cifar-10-batches-py\data_batch_5'
+bytestream=open(filename,'rb')
+buf=bytestream.read(10000*(1+32*32*3))
+bytestream.close()
+
+data=np.frombuffer(buf,dtype=np.uint8)
+data=data.reshape(10000,1+32*32*3)
+labels_images=np.hsplit(data,[1])
+labels_=labels_images[0].reshape(10000)
+images_=labels_images[1].reshape(10000,32,32,3)
+
+img=np.reshape(images_[5200],(3,32,32))
+img=img.transpose(1,2,0)
+pylab.imshow(img[...,0])
+pylab.show()
