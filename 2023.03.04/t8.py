70eb25a78f51f612b811ac8004405a69023d2375
lsm232 <1255567784@qq.com>
2023-03-09 23:23:18 +0800

    bad

---
+++ 2023.03.04/t8.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+strps_hosts='localhost:1681'
+strworkers_hosts='localhost:1682,localhost:1683'
+
+ps_hosts=strps_hosts.split(',')
+worker_hosts=strworkers_hosts.split(',')
+c=1
