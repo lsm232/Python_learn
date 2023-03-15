ab9300fbad592b607c077e4ee6421712c06d0905
lsm232 <1255567784@qq.com>
2023-03-15 09:40:06 +0800

    bad

--- 2023.03.04/t9.py
+++ 2023.03.04/t9.py
@@ -28,16 +28,32 @@ acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 optimizer=tf.train.GradientDescentOptimizer(0.001).minimize(cost)
 
 init=tf.global_variables_initializer()
+saver=tf.train.Saver()
+# with tf.Session() as sess:
+#     sess.run(init)
+#     total_batch=int(mnist.train.images.shape[0]/batch_size)
+#     for epoch in range(epochs):
+#         avg_cost=0.
+#         for i in range(total_batch):
+#             batchx,batchy=mnist.train.next_batch(batch_size)
+#             _,c=sess.run([optimizer,cost],feed_dict={X:batchx,Y:batchy})
+#             avg_cost+=c/total_batch
+#         if (epoch+1)%display_freq==0:
+#             print("epoch=%d"%(epoch+1),'cost={:.4f}'.format(avg_cost))
+#     print('ok')
+#     print('acc=',acc.eval({X:mnist.validation.images,Y:mnist.validation.labels}))
+#
+#     save_path=saver.save(sess,'log/model1.ckpt')
+
 with tf.Session() as sess:
     sess.run(init)
-    total_batch=int(mnist.train.images.shape[0]/batch_size)
-    for epoch in range(epochs):
-        avg_cost=0.
-        for i in range(total_batch):
-            batchx,batchy=mnist.train.next_batch(batch_size)
-            _,c=sess.run([optimizer,cost],feed_dict={X:batchx,Y:batchy})
-            avg_cost+=c/total_batch
-        if (epoch+1)%display_freq==0:
-            print("epoch=%d"%(epoch+1),'cost={:.4f}'.format(avg_cost))
-    print('ok')
-    print('acc=',acc.eval({X:mnist.validation.images,Y:mnist.validation.labels}))
+    saver.restore(sess,'./log/model1.ckpt')
+    print('acc=',sess.run([acc],feed_dict={X:mnist.validation.images,Y:mnist.validation.labels}))
+
+    output=tf.argmax(pred,1)
+    batch_xs,batch_ys=mnist.train.next_batch(2)
+    outputval=sess.run([output],feed_dict={X:batch_xs})
+    print('pred:',outputval,'label:',batch_ys)
+
+    pylab.imshow(batch_xs[1].reshape(-1,28))
+    pylab.show()
