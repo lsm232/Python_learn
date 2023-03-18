2c7c818743080130a4b3dd4a5660002a73bd9f41
lsm232 <1255567784@qq.com>
2023-03-18 23:17:08 +0800

    bad

--- 2023.03.04/t11.py
+++ 2023.03.04/t11.py
@@ -1,3 +1,14 @@
-import numpy as np
-x=np.random.random([4,4])
-for i,j in product()
+import tensorflow as tf
+
+global_step=tf.Variable(0,False)
+add_step=global_step.assign_add(1)
+lr_rate=1.
+lr_rate_decay=tf.train.exponential_decay(lr_rate,global_step,decay_steps=10,decay_rate=0.5)
+
+init=tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(1,12):
+        lr,step=sess.run([lr_rate_decay,add_step])
+        print(lr,step)
