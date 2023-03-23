57ccf5b59665834f4d811638eeb1ad7d4485d523
lsm232 <1255567784@qq.com>
2023-03-23 23:13:35 +0800

    bad

--- 2023.03.04/t13.py
+++ 2023.03.04/t13.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from random import shuffle
 # x=np.random.random([2])
@@ -30,6 +31,43 @@ mean=np.random.randn(num_dims)
 cov=np.eye(num_dims)
 X,Y=generate(1000,mean,cov,[3.0],True)
 c=1
-colors=['r' if l==0 else 'b' for l in Y ]
-plt.scatter(X[:,0],X[:,1],c=colors)
-plt.show()
+# colors=['r' if l==0 else 'b' for l in Y ]
+# plt.scatter(X[:,0],X[:,1],c=colors)
+# plt.show()
+
+input_features=tf.placeholder(dtype=tf.float32,shape=[None,2])
+input_labels=tf.placeholder(dtype=tf.float32,shape=[None,1])
+
+w=tf.Variable(tf.random_normal([2,1]),name='weight')
+b=tf.Variable(tf.zeros([1]),name='bias')
+pred=tf.matmul(input_features,w)+b
+out=tf.nn.sigmoid(pred)
+loss=tf.reduce_mean(-(input_labels*tf.log(out)+(1-input_labels)*tf.log(1-out)))
+optimizer=tf.train.AdamOptimizer(0.002).minimize(loss)
+
+
+epochs=100
+batchs=10
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(epochs):
+        err=0.
+        for inter in range(int(len(Y)/batchs)):
+            x1=X[inter*batchs:(inter+1)*batchs,:]
+            y1=Y[inter*batchs:(inter+1)*batchs].reshape(-1,1)
+            _,cost=sess.run([optimizer,loss],feed_dict={input_features:x1,input_labels:y1})
+            err+=cost/len(Y)
+        print('epoch:',err)
+
+
+    trainx,trainy=generate(100,mean,cov,[3.0],True)
+    colors=['g' if l==0 else 'b'   for l in trainy]
+    plt.scatter(trainx[:,0],trainx[:,1],c=colors)
+    x=np.linspace(-1,8,200)
+    y=-x*(sess.run(w)[0]/sess.run(w)[1])-sess.run(b)/sess.run(w)[1]
+    plt.plot(x,y)
+    plt.show()
+
+
