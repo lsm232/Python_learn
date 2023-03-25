1edfe6dfd8e176e2e4c132a56fb9e0a49dd6af1b
lsm232 <1255567784@qq.com>
2023-03-25 22:48:11 +0800

    bad

--- 2023.03.04/t14.py
+++ 2023.03.04/t14.py
@@ -1,6 +1,20 @@
-w=tf.Variable(tf.random_normal([2,1]),name='weight')
-b=tf.Variable(tf.zeros([1]),name='bias')
-pred=tf.matmul(input_features,w)+b
-out=tf.nn.sigmoid(pred)
-loss=tf.reduce_mean(-(input_labels*tf.log(out)+(1-input_labels)*tf.log(1-out)))
-optimizer=tf.train.AdamOptimizer(0.002).minimize(loss)
+import numpy as np
+import tensorflow as tf
+
+def generate(sample_size,mean,cov,diff,regression,num_class):
+    sampls_per_class=int(sample_size/num_class)
+    x0=np.random.multivariate_normal(mean,cov,sampls_per_class)
+    y0=np.zeros(sampls_per_class)
+
+    for ci,d in enumerate(diff):
+        x1=np.random.multivariate_normal(mean+d,cov,sampls_per_class)
+        y1=ci+1+y0
+
+        x=np.concatenate([x0,x1])
+        y=np.concatenate([y0,y1])
+
+    if regression==False:
+        class_ind=[y0==class_number for class_number in range(num_class)]
+        y=np.asarray(np.hstack([class_ind]),dtype=np.float32)
+    return x,y
+
